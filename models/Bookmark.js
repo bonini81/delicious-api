6f0bfde9ec0b4542138e3a42f3be59bc775d2132
bonini81 <andresdominguez81@gmail.com>
2026-06-17 09:06:00 -0500

    Added category on bookmark data model

--- models/Bookmark.js
+++ models/Bookmark.js
@@ -6,6 +6,7 @@ const bookmarkSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, default: '' },
   tags: { type: [String], default: [] },
+  category: { type: String, default: '' },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Bookmark', bookmarkSchema);
